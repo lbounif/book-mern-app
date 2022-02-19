dcfef99f2b5634432a96ccb4a1130d665ec7475f
Lynda <l_bounif@esi.dz>
2022-02-19 11:51:06 +0100

    change mongo url for deployment

--- book-api/src/db/mongoose.js
+++ book-api/src/db/mongoose.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose")
 // .env
 require("dotenv").config()
 
-const MONGODB_URL = `mongodb://${process.env.DATABASE_HOST}:
-${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`
+// const MONGODB_URL = `mongodb://${process.env.DATABASE_HOST}:
+// ${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`
+
+const MONGODB_URL = "mongodb://mongo:27017/book-kubernetes-app"
 
 mongoose.connect(MONGODB_URL, {
   useUnifiedTopology: true,
